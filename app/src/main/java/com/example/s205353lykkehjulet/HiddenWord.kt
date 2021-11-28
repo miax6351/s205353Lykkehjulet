2159940a05e3a9c1c3d83b20b29a4d2e7bc731ae
Mia <miadong179@gmail.com>
2021-11-28 19:02:27 +0100

    Kan ikke gætte to gangen u

--- app/src/main/java/com/example/s205353lykkehjulet/HiddenWord.kt
+++ app/src/main/java/com/example/s205353lykkehjulet/HiddenWord.kt
@@ -1,20 +1,34 @@
 package com.example.s205353lykkehjulet
 
+
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 import kotlin.random.Random
+
 
 object HiddenWord {
 
     private var questionMarkArray: ArrayList<Char> = ArrayList()
     private var hiddenWordsArray = arrayOf("Cat","Dog","Rooster","Coca Cola")
-    private var word = hiddenWordsArray[0]
-    //private var word = hiddenWordsArray[(Random.nextInt(0, hiddenWordsArray.size - 1))]
+    private var word = hiddenWordsArray[(Random.nextInt(0, hiddenWordsArray.size - 1))]
     private var wordArray = word.toList() as ArrayList<Char>
+    private var guessedMap = HashMap<Char, Boolean>()
     private var rightGuesses = 0;
     private var letterIsRight : Boolean = false
 
 
+    init {
+        setGuessedArray()
+    }
+
     fun getHiddenWordArray(): ArrayList<Char> {
         return wordArray
+    }
+
+    fun setGuessedArray(){
+        for (i in 0..wordArray.size - 1){
+            guessedMap.put(wordArray[i],false)
+        }
     }
 
     fun displayLetterIfTrue(guess : String){
@@ -23,16 +37,13 @@ object HiddenWord {
 
         for (i in 0..wordArray.size - 1) {
             if (guess.equals(wordArray[i].toString(),true)){
-                getQuestionMarkArray()[i] = wordArray[i]
-                letterIsRight = true
-                rightGuesses++
+                if (!guessedMap.getValue(wordArray[i])){
+                    getQuestionMarkArray()[i] = wordArray[i]
+                    letterIsRight = true
+                    rightGuesses++
+                    guessedMap.put(wordArray[i],true)
+                }
             }
-        }
-        // To ensure you only get points for one letter at a time
-        for (i in (wordArray.size - 1) downTo 0){
-//            if (wordArray[i].equals(getQuestionMarkArray()[i]))
-//                wordArray[i] = ' '
-
         }
 
     }
